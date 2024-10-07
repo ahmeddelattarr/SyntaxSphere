78c9ceb44cc15e04ab567c25c62303e0c333fe48
0marAmara <onan383@outlook.com>
2024-10-08 00:05:33 +0300

    [refactor] refactored  a new interface

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,31 +1,15 @@
-import { useLocation} from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Navbar from "../components/ui/Navbar";
 import Post from "../components/ui/Post";
 import NewPostForm from "../components/ui/NewPostForm";
 import Pagination from "../components/ui/Pagination";
 import useFetchWithToken from "../hooks/useFetchWithToken";
+import { PostResponse } from "../types/post-interfaces";
 
-interface PostData {
-  id: string;
-  title: string;
-  content: string;
-  url: string;
-  user: string;
-  posted_at: string;
-  like_count: number;
-}
-
-interface PostResponse {
-  previous: string | null;
-  next: string | null;
-  count: number;
-  results: PostData[];
-}
 
 const getPageNumber = (pathname: string) => {
   return pathname === '/' ? 1 : parseInt(pathname.split('/')[1]);
 };
-
 
 const Homepage = () => {
   const location = useLocation();
@@ -35,7 +19,7 @@ const Homepage = () => {
 
   const posts = postResponse?.results;
 
-  const PostsEl = posts&&<div className="flex flex-col">{posts.map((post, i, posts) => (<Post isLast={i == posts.length - 1} key={post.id} post={post} />))}</div>;
+  const PostsEl = posts && <div className="flex flex-col">{posts.map((post, i, posts) => (<Post isLast={i == posts.length - 1} key={post.id} post={post} />))}</div>;
 
   return (
     <div className="min-h-screen w-full text-gray-200 bg-gray-800">
@@ -49,7 +33,7 @@ const Homepage = () => {
             <p className="text-center text-gray-400">No posts available</p>
           )}
         </div>
-        <Pagination refreshTimeLine={refreshTimeLine} currentPage={currentPage} totalPosts={postResponse?.count||0} />
+        <Pagination refreshTimeLine={refreshTimeLine} currentPage={currentPage} totalPosts={postResponse?.count || 0} />
       </div>
     </div>
   );
